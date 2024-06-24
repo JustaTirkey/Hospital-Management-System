44c60f28776079e268d2239f9008ec6e55d45f1f
Justa Tirkey <tirkeyjusta2001@gmail.com>
2024-06-24 22:57:01 +0530

    DBMS Project first commit

---
+++ src/hospital_management/Hospital_Management.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospital_management;
+
+/**
+ *
+ * @author tirke
+ */
+public class Hospital_Management {
+     /**
+     * Main method to start the hospital management application.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // You can add initialization code or call other classes/methods here
+        System.out.println("Hospital Management Application Started");
+        
+       MainPage mainPage = new MainPage();
+       mainPage.setVisible(true);
+    }
+}
